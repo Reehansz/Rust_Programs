5583d384a2cc7ccf9cf222c1932f7ea0c1b24f69
Reehan Shaveez <reehanshaveez123@gmail.com>
2024-07-30 15:42:24 +0530

    Moved day1 branch files into day1 folder

--- src/string.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let company:&str = "INFOSYS";
-    let location:&str = "Hyderabad";
-    println!("Company is={} location:{}",company,location);
-}
--- src/string_object2.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let empty_string = String::new();
-    println!("length is {}",empty_string.len());
-
-    let content_string = String::from("Infosys");
-    println!("lenght is {}",content_string.len());
-}
